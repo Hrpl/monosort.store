3ba786863bce7f916b4586d57215c9baf3e11d34
Denis <ponomarevd61@gmail.com>
2025-05-29 17:16:23 +0300

    fix url 2

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setDescription('')
     .setVersion('1.0')
     .addTag('')
-    .addServer('https://storage.monosortcoffee.ru/api')
+    .addServer('/api/')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, documentFactory);
