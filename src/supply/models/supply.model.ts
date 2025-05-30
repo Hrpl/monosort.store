464184bb716da097b69866d6030e806732349b2b
Denis <ponomarevd61@gmail.com>
2025-05-30 12:44:48 +0300

    edit decorator

--- src/supply/models/supply.model.ts
+++ src/supply/models/supply.model.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { SupplyProductModel } from './supply.product.mode';
+import { Column } from 'typeorm';
 
 export class SupplyModel {
   @ApiProperty()
+  @Column()
   date: string;
 
   @ApiProperty({
--- src/supply/models/supply.product.mode.ts
+++ src/supply/models/supply.product.mode.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Column } from 'typeorm';
 
 export class SupplyProductModel {
   @ApiProperty()
+  @Column()
   count: number;
 
   @ApiProperty()
+  @Column()
   supplyId: number;
 
   @ApiProperty()
+  @Column()
   productId: number;
 }
